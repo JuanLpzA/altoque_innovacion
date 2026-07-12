5e53535c3ced7132eb030ea8e7fedfa44972f394
Juan Lopez <lopezarrunategui2@gmail.com>
2026-07-12 17:20:22 -0500

    Mejoras finales. Listo para pruebas de usuario

--- src/main/java/com/innovacion/altoque/model/Reniec.java
+++ src/main/java/com/innovacion/altoque/model/Reniec.java
@@ -1,10 +1,25 @@
 package com.innovacion.altoque.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "reniec")
 public class Reniec {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 8, columnDefinition = "CHAR(8)")
     private String dni;
+
+    @Column(nullable = false, length = 100)
     private String nombres;
+
+    @Column(nullable = false, length = 100)
     private String apellidos;
+
+    @Column(name = "fecha_consulta", updatable = false)
+    private LocalDateTime fechaConsulta = LocalDateTime.now();
 }
